1663583caa937db150e3371735d7b1f74cb2773c
SonHai-code <haisp2002@gmail.com>
2022-07-13 09:41:39 +0700

    Protecting tour-router p1

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -9,13 +9,14 @@ const {
   getTourStats,
   getMonthlyPlan,
 } = require('./../controllers/tourController');
+const { protect } = require('./../controllers/authController');
 const router = express.Router();
 
 // router.param('id', checkID);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-tours').get(aliasTopTours, getAllTours);
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').delete(deleteTour).patch(updateTour).get(getTour);
 
 module.exports = router;
