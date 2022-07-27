dc3ad648a6d7df8c54976088f06eb592436e2be4
SonHai-code <haisp2002@gmail.com>
2022-07-27 21:21:43 +0700

    Some modifiers

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,7 +33,21 @@ app.use(express.json());
 app.use(mongoSanitize());
 
 // Data sanitization against XSS
-app.use(xss()); 
+app.use(xss());
+
+// Prevent parameters polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
